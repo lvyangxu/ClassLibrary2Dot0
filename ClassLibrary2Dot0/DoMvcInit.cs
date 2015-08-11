aad4158614b12e9ee74b38a63d48bbc10555cb48
lvyangxu <285672259@qq.com>
2015-08-11 11:01:38 +0800

    2015-08-11

---
+++ ClassLibrary2Dot0/DoMvcInit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+
+namespace ClassLibrary2Dot0
+{
+    public class DoMvcInit
+    {
+        DoIO DoIO1 = new DoIO();
+
+        public void InitMvcWebFrontEnd(HttpServerUtility Server) {
+            DoIO1.directoryCopy(Server.MapPath("bin/js/"), Server.MapPath("Content/js/"));
+            DoIO1.directoryCopy(Server.MapPath("bin/css/"), Server.MapPath("Content/css/"));
+        }
+    }
+}
